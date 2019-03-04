14570b7911294ad9583a387285178bb3f7a7234a
koki0419 <azunyan40.19@gmail.com>
2019-03-04 14:41:15 +0900

    [update]Update CgargeGage
    commit 3fcbe4570df6b7e7fcb07dd4e01d27ea5ed447a7
    Author: koki0419 <azunyan40.19@gmail.com>
    Date:   Mon Mar 4 13:01:45 2019 +0900
    
        [update]Update CgargeGage
    
    commit f03ae34951f3f4dd0cb40abc72f6703c9c9155f4
    Author: koki0419 <azunyan40.19@gmail.com>
    Date:   Sun Mar 3 05:50:25 2019 +0900
    
        [change]Change Player_DestroyMode
    
    commit 3970b452a5f9738fc9824c90c49468f4b6d2eda9
    Author: koki0419 <azunyan40.19@gmail.com>
    Date:   Sun Mar 3 00:12:42 2019 +0900
    
        [add]Add StarChargeController.cs
        [change]Change ChargeGage

---
+++ UnityProjct/Assets/Scripts/StarState.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StarState : MonoBehaviour
+{
+    //大きい☆使用状態
+    public enum Star
+    {
+        None,
+        DontUse = 1001,
+        Use = 1010,
+        Used = 1011,
+        Chage = 1100,//チャージ中（大きい☆用）
+        NotAvailable = 9999,//使用禁止（大きい☆用）
+    }
+    public Star star = Star.None;
+
+    [Header("☆の状態画像")]
+    [SerializeField] Sprite dontUseStarSprite;//使っていない
+    [SerializeField] Sprite useStarSprite;//使っている
+    [SerializeField] Sprite usedStarSprite;//使い終わった
+    [SerializeField] Sprite chageStarSprite;//チャージ中（大きい☆用）
+    [SerializeField] Sprite notAvailableStarSprite;//使用禁止（大きい☆用）
+
+    [SerializeField] Image starImage; 
+
+
+    public void UpdateStarSprite(int starState)
+    {
+        switch (starState)
+        {
+            case (int)Star.DontUse:
+                starImage.sprite = dontUseStarSprite;
+                break;
+            case (int)Star.Use:
+                starImage.sprite = useStarSprite;
+                break;
+            case (int)Star.Used:
+                starImage.sprite = usedStarSprite;
+                break;
+            case (int)Star.Chage:
+                starImage.sprite = chageStarSprite;
+                break;
+            case (int)Star.NotAvailable:
+                starImage.sprite = notAvailableStarSprite;
+                break;
+        }
+    }
+}
