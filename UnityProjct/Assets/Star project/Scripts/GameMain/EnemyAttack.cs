1bae69b636f3819bb9c841a933ce8a21db2bbd1e
KamiganeYuuki <yatakarasu1994@gmail.com>
2019-04-11 17:44:20 +0900

    [update]Update PrototypeScene.unity
    commit e1f80c49d323539d15d406c8b24d1942243f9549
    Author: KamiganeYuuki <yatakarasu1994@gmail.com>
    Date:   Thu Apr 11 17:42:49 2019 +0900
    
        [update]Update PrototypeScene.unity
    
    commit da423df4f41255998b87b28ec785778a0b1b398f
    Author: KamiganeYuuki <yatakarasu1994@gmail.com>
    Date:   Thu Apr 11 13:40:49 2019 +0900
    
        [update]Update ResultScene.unity

---
+++ UnityProjct/Assets/Star project/Scripts/GameMain/EnemyAttack.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EnemyAttack : MonoBehaviour
+{
+
+    GameObject Player;
+
+    ChargePointManager script;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Player = GameObject.Find("Player");
+        script = Player.GetComponent<ChargePointManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int starCount = script.StarChildCount;
+
+    }
+
+
+
+    private void OnTriggerExit(Collider collision)
+    {
+
+
+        if (collision.tag == "Player")
+        {
+
+
+/*            starCount = 0;
+            if (starCount == 0)
+            {
+                SceneManager.LoadScene("ResultScene");
+            }*/
+        }
+    }
+
+}
