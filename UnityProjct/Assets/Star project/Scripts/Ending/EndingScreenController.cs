6fbbe554a1fb8a8d2b5ddfa81d7be6fc7d776706
koki0419 <azunyan40.19@gmail.com>
2019-04-12 13:59:26 +0900

    [update]Update build

--- UnityProjct/Assets/Star project/Scripts/Ending/EndingScreenController.cs
+++ UnityProjct/Assets/Star project/Scripts/Ending/EndingScreenController.cs
@@ -14,9 +14,9 @@ public class EndingScreenController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Return))
         {
-            SceneManager.LoadScene("SelectScene");
+            SceneManager.LoadScene("TitleScene");
         }
     }
 }
