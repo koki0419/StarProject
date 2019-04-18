c5c9e8f2259c05e38b7e6ec29ab82df6ed3b3cab
koki0419 <azunyan40.19@gmail.com>
2019-04-18 23:12:55 +0900

    [add]Add new Scenes/Player_Test.unity
    commit 4809bda0dbfe4592081a85b398736eb35402f9aa
    Author: koki0419 <azunyan40.19@gmail.com>
    Date:   Thu Apr 18 22:58:39 2019 +0900
    
        [add]Add new Scenes/Player_Test.unity
        [add]Add Player.cs

---
+++ UnityProjct/Assets/Test/Player.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    [SerializeField] float moveSpeed;
+    [SerializeField] float jumpSpeed;
+    [SerializeField] float gravity;
+
+    private Vector3 charaMove;
+    bool isGround;
+    new Rigidbody rigidbody;
+    CharacterController characterController;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+        characterController = GetComponent<CharacterController>();
+        charaMove = Vector3.zero;
+        isGround = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       float horizontal = Input.GetAxis("Horizontal");
+
+        charaMove.x = horizontal * moveSpeed;
+
+        if (characterController.isGrounded)
+        {
+            if (Input.GetButtonDown("Jump"))
+            {
+                charaMove.y = jumpSpeed;
+            }
+            charaMove.y += 0;
+        }
+        else
+        {
+            charaMove.y += Physics.gravity.y * gravity * Time.deltaTime;
+        }
+        characterController.Move(charaMove * Time.deltaTime);
+        Debug.Log("charaMove : " + charaMove);
+    }
+
+    //private void FixedUpdate()
+    //{
+    //    float horizontal = Input.GetAxis("Horizontal");
+    //    //Debug.Log("horizontal  = " + horizontal);
+    //    charaMove.x = horizontal * moveSpeed;
+    //    if (characterController.isGrounded)
+    //    {
+    //        if (Input.GetButtonDown("Jump"))
+    //        {
+    //            charaMove.y -= Physics.gravity.y * gravity * Time.fixedDeltaTime;
+    //            Debug.Log("Jump");
+    //        }
+    //        charaMove.y = 0;
+    //    }
+    //    else
+    //    {
+    //        Debug.Log("horizontal  = ");
+    //        charaMove.y += Physics.gravity.y * gravity * Time.fixedDeltaTime;
+    //    }
+    //    characterController.Move(charaMove * Time.fixedDeltaTime);
+    //    Debug.Log("isGrounded" + characterController.isGrounded);
+    //}
+
+}
