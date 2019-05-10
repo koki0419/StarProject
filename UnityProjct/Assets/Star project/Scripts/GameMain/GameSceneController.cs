84af49d6dc9e20682ac476df6c293dbbfadbbbc1
koki0419 <azunyan40.19@gmail.com>
2019-05-10 18:38:52 +0900

    [update]Update mainStage remake
    commit 195eb2d66e4443b7998fd4986689c3d28cb55048
    Author: koki0419 <azunyan40.19@gmail.com>
    Date:   Fri May 10 18:36:51 2019 +0900
    
        [update]Update mainStage remake

--- UnityProjct/Assets/Star project/Scripts/GameMain/GameSceneController.cs
+++ UnityProjct/Assets/Star project/Scripts/GameMain/GameSceneController.cs
@@ -219,7 +219,7 @@ namespace StarProject.Gamemain
             ResultScreenController.allStar = chargePointManager.starChildCount;
             yield return new WaitForSeconds(0.5f);
             uiManager.GameClearUIDisplay(true);
-            yield return new WaitForSeconds(4.5f);
+            yield return new WaitForSeconds(3.0f);
             canCameraShake = false;
             SceneManager.LoadScene("ResultScene");
 
--- UnityProjct/Assets/Star project/Scripts/GameMain/StarController.cs
+++ UnityProjct/Assets/Star project/Scripts/GameMain/StarController.cs
@@ -7,12 +7,19 @@ public class StarController : MonoBehaviour
 
     //---------Unityコンポーネント宣言--------------
     GameObject playerObj;
+    [SerializeField] private Material goldStarMaterial;//黄金マテリアル   ☆獲得ポイント3
+    [SerializeField] private Material silverStarMaterial;//銀色マテリアル   ☆獲得ポイント2
+    [SerializeField] private Material bronzeStarMaterial;//銅色マテリアル   ☆獲得ポイント1
 
+    [SerializeField] private Renderer starRenderer;
     //------------クラスの宣言----------------------
     PlayerMove playerMove;
     public StarGenerator starGenerator;
     //------------数値変数の宣言--------------------
     [SerializeField] private int starPoint;
+    [SerializeField] private int goldStarPoint;
+    [SerializeField] private int silverStarPoint;
+    [SerializeField] private int bronzeStarPoint;
     //------------フラグ変数の宣言------------------
     public enum StarSponType
     {
@@ -28,6 +35,17 @@ public class StarController : MonoBehaviour
     {
         playerMove = playermove;
         starPoint = point;
+
+        if(starPoint == goldStarPoint)//黄金の☆マテリアル
+        {
+            starRenderer.material = goldStarMaterial;
+        }else if(starPoint == silverStarPoint)//銀色の☆マテリアル
+        {
+            starRenderer.material = silverStarMaterial;
+        }else if(starPoint == bronzeStarPoint)//銅色の☆マテリアル
+        {
+            starRenderer.material = bronzeStarMaterial;
+        }
     }
 
     private void Update()
